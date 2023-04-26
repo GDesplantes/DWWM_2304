9eb1f01464c9366a68102540d23fa6497da25937
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-04-26 09:32:24 +0200

    Fin première partie Exercice Algo

--- Algo/ExoAlgo/ExoAlgo05/Program.cs
+++ Algo/ExoAlgo/ExoAlgo05/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            int A;
+            int B;
+            int C=0;
+
+            Console.WriteLine("Veuillez saisir la valeur de A");
+
+            A=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez saisir la valeur de B");
+
+            B=int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("La valeur de A est de " + A + " et de B est de " + B);
+
+            C = B;
+            B = A;
+            A = C;
+
+            Console.WriteLine("Invertion des nombres en cours . . . .");
+
+            Console.WriteLine("Les nouvelles valeurs de A et B sont " + A + " et " +B);
+
         }
     }
 }
