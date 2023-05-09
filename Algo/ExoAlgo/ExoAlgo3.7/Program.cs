813381cd9b9f37366f1744976ff2a2a902fb12bd
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-05-09 16:55:22 +0200

    ExoAlgoPart4

--- Algo/ExoAlgo/ExoAlgo3.7/Program.cs
+++ Algo/ExoAlgo/ExoAlgo3.7/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float argentRestant;
+            int nbMagasins = 0;
+
+           
+         
+            Console.WriteLine("Saisissez l'argent de barnabé");
+
+            argentRestant = float.Parse(Console.ReadLine());
+
+            while (argentRestant < 1)
+            {
+                Console.WriteLine("Saisissez l'argent de barnabé");
+
+                argentRestant = float.Parse(Console.ReadLine());
+            }
+
+            if(argentRestant>1 && argentRestant < 2)
+            {
+                Console.WriteLine("Barnabé n'a pas asser d'argent pour faire les magasins");
+            }
+
+         
+           
+            while (argentRestant >= argentRestant/2 + 1 )
+            {
+                argentRestant = argentRestant / 2 - 1; ;
+                nbMagasins++;
+            }
+
+
+            Console.WriteLine("Barnabé n'a plus que " + argentRestant + " euros, il a put cependant aller dans " + nbMagasins + " magasins");
         }
     }
 }
---
+++ Algo/ExoAlgo/ExoAlgo4.1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ExoAlgo4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nombres = { 8, 16, 32, 64, 128, 256, 512};
+            int cherche;
+            int i = 0;
+
+            Console.WriteLine("Chercher un nombre dans le tableaux");
+
+            cherche=int.Parse(Console.ReadLine());
+
+            do
+            {
+                i++;
+            }
+            while (i < nombres.Length);
+
+
+            /*for (int i = 0; i < nombres.Length; i++)
+            {
+                int resulat = nombres[i];
+                if (resulat == cherche)
+                {
+                    Console.WriteLine(cherche + " est présents dans le tableaux");
+                }
+                else
+                {
+                    Console.WriteLine(cherche + "");
+                }
+            }*/
+            
+
+
+        }
+    }
+}
