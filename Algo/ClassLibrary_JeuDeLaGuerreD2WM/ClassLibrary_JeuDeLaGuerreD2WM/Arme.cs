c2fffd1de3bcc341b74e7909b4ff7d03249d5989
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-07-06 16:55:23 +0200

    .

---
+++ Algo/ClassLibrary_JeuDeLaGuerreD2WM/ClassLibrary_JeuDeLaGuerreD2WM/Arme.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_JeuDeLaGuerreD2WM
+{
+    internal class Arme
+    {
+        private double puissanceArme;
+        private string nomArme;
+        public Arme()
+        {
+            this.puissanceArme = 55;
+            this.nomArme = "Berta";
+        }
+
+        public Arme(double _puissanceArme, string _nomArme)
+        {
+            this.puissanceArme = _puissanceArme;
+            this.nomArme = _nomArme;
+        }
+
+        
+    }
+}
---
+++ Algo/ClassLibrary_JeuDeLaGuerreD2WM/ClassLibrary_JeuDeLaGuerreD2WM/Guerrier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_JeuDeLaGuerreD2WM
+{
+    internal class Guerrier
+    {
+
+        private string nom;
+        private int nombreVies;
+
+
+        public Guerrier(string _nom, int _nombreVies, int _nombresArmes, double _puissanceUneArme)
+        {
+            this.nom = _nom;
+            this.nombreVies = _nombreVies;
+        }
+
+        public Guerrier()
+        {
+            this.nom = "Celestin";
+            this.nombreVies = 10;
+
+        }
+
+        public bool Affronttement(Guerrier autreGuerrier)
+        {
+
+        }
+    }
+}
