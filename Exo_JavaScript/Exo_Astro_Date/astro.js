d8d346d07ce1d61b01c234c3cf5d0b7fa6c8ecae
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-09-29 11:51:56 +0200

    push

--- Exo_JavaScript/Exo_Astro_Date/astro.js
+++ Exo_JavaScript/Exo_Astro_Date/astro.js
@@ -1,5 +1,36 @@
+
 function valNum(_maChaine)
 {
-    let somme;
+    let somme = 0;
+    let chaineMaj = _maChaine.toUpperCase();
+
+
+    for(let i = 0; i < chaineMaj.length; i++)
+    {
+        
+        somme += chaineMaj.charCodeAt(i) - 64;
+    }
+
+    return somme;
 
 }
+
+console.log(valNum("guillaume")); 
+
+
+
+function calculerSigne(_mois)
+{
+     let moisSigne = _mois
+
+    let moisNaissance = ["Capricorne","Verseau","Poisson","Belier","Taureau","Gémeaux","Cancer","Lion","Vierge","Balance","Scorpion","Sagittaire"]
+
+    for(let i = 0; i < moisNaissance.length; i++ )
+    {
+        moisSigne=moisNaissance[i]
+    }
+    return _mois
+
+}
+
+console.log(calculerSigne(5));
