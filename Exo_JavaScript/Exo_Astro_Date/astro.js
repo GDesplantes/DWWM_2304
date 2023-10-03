d0f4a9aea151315cadb331cc9751b7725c624362
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-10-03 15:07:39 +0200

    push

--- Exo_JavaScript/Exo_Astro_Date/astro.js
+++ Exo_JavaScript/Exo_Astro_Date/astro.js
@@ -1,11 +1,36 @@
 function remplirAnnee()
 {
-    for(i=1915;i<=2015;i++)
+    for(i=1953;i<=2005;i++)
     {
-        document.getElementById("annee").innerHTML+='<option>'+i+'</option>';
+        document.getElementById("annee").innerHTML+='<option value= \"'+i+'\">'+i+'</option>';
         
     }
 }
+
+let tabMois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
+
+function afficherMois()
+{
+    let monOption = document.createElement('option'); //création d'une balise "option"
+    monOption.value="";
+    monOption.textContent = "Choisir le mois"; //contenue texte
+    document.getElementById("mois").appendChild(monOption);// on place la balise option dans le select "mois"
+
+    for (let i=0 ; i<tabMois.length; i++)
+    {
+        monOption = document.createElement('option') // on recréé une succession d'élément option
+        monOption.value= i+1; // on attribut une valeur a monOption
+        // if (i+1 == 5) 
+        // {
+        //     monOption.setAttribute('selected', 'true')  Placer le mois de mais par défaut
+        // }
+        monOption.textContent =tabMois[i]; // contenue texte de mon option --> string a l'indice i du tableau
+        document.getElementById("mois").options[i+1] = monOption; //
+    }
+
+
+}
+
 function valNum(_maChaine)
 {
     let somme = 0;
@@ -36,8 +61,10 @@ function calculerSigne(_mois)
 
 console.log(calculerSigne(7));
 
-console.log(document.prenomUtilisateur.value)
+//console.log(document.prenomUtilisateur.value)
 
 
+remplirAnnee()
 
+afficherMois()
 
