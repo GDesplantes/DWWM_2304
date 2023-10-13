d76da2603fb977b22d307e59966f00793d03c2d4
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-10-13 11:51:22 +0200

    push js

---
+++ Exo_JavaScript/Exo_Astro_Date/intervalles temporelles/intervalles_temporelles.js
@@ -0,0 +1,21 @@
+const btnAffichageDateHeure = document.getElementById("AfficherDateHeure");
+const heureActuelle = document.getElementById("idHeureActuelle");
+const dateActuelle = document.getElementById("idDateActuelle");
+const textDateHeure = document.getElementById("texteDateEtHeure");
+
+
+
+
+
+
+
+
+
+
+
+
+btnAffichageDateHeure.addEventListener('click', function (){
+
+    
+
+});
