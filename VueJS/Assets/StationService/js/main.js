730cfe4b96e2770e306c918ed1aad48e1cef9661
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-10-17 16:55:40 +0200

    vueJS

---
+++ VueJS/Assets/StationService/js/main.js
@@ -0,0 +1,24 @@
+const { createApp } = Vue;
+
+const appExemple = {
+    data() {
+        return  {
+            prixAuLitre: 0,
+            quantiteEnLitre: 0,
+            prixAPayer: 0
+            
+        }
+    },
+    computed: {
+
+
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    }
+}
+
+createApp(appExemple).mount('#app');
