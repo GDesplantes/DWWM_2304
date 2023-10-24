656903a674cd1cde736279e311fc81e46e1cf22e
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2023-10-24 15:57:56 +0200

    fini pompe à essence

--- VueJS/Assets/StationService/js/main.js
+++ VueJS/Assets/StationService/js/main.js
@@ -4,40 +4,56 @@ const appExemple = {
     data() {
         return  {
             prixAuLitre: 1.65,
-            quantiteEnLitre: 2,
+            inputReadonly: false,
+            quantiteEnLitre: 0,
             btnStart: false,
             libeleBtn: "Start",
             intervalle: null
             
         }
     },
-    computed: {
-
-        calculPrix() {
-            return (this.prixAuLitre*this.quantiteEnLitre).toFixed(2)
-        }
-
-
-    },
-    mounted() {
+    computed: { //  : Object
         
+
+        calculPrix() { // fonction --> chaîne 
+            if(this.prixAuLitre > 0) {
+                return (this.prixAuLitre*this.quantiteEnLitre).toFixed(2)
+            } else {
+                return "Prix au litre invalide"
+            }
+        },
+
+        quantiteArrondie() {
+            return this.quantiteEnLitre.toFixed(2)
+
+        },
     },
     methods: {
 
-    calculerPrix() {
-        if(this.btnStart) {
-            this.libeleBtn = "Start"
-            clearInterval(this.intervalle)
-        } else {
-            this.quantiteEnLitre = 0;
-            this.libeleBtn = "Stop"
-        }
+        calculerPrix() {
+            if(this.btnStart) {
+                this.libeleBtn = "Start"
+                clearInterval(this.intervalle)
+            } else {
+                this.quantiteEnLitre = 0;
+                this.libeleBtn = "Stop"
+                this.intervalle = setInterval(() => {
+                    this.quantiteEnLitre += 0.09;
+                }, 50);
+            }
+        
 
-    this.distribution = !this.distribution
-    }
-    
+        this.btnStart = !this.btnStart
+        },
+        reset() {
+            this.quantiteEnLitre = 0;
+            
+        }
+        
+        
     }
     
 }
 
-createApp(appExemple).mount('#app');
+
+Vue.createApp(appExemple).mount('#app');
